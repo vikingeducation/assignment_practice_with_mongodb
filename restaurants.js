2629a041b5107120e519c57894bb65946b7011fb
deven1 <deven@sloan.mit.edu>
2017-04-13 19:49:31 -0400

    updating unwind

--- restaurants.js
+++ restaurants.js
@@ -4,6 +4,5 @@ Sum the scores for each restaurant
 
 db.restaurants.aggregate([
   { $unwind: "$grades" },
-  { $project: { _id: 0, name: 1 } },
-  { $group: { _id: "$grades", sum: { $sum: "$score" } } }
+  { $group: { _id: "$name", sum: { $sum: "$grades.score" } } }
 ]);
