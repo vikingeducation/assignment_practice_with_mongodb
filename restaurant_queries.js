9516c5acd07173b0b166d450b92056b82923d866
rabGIT <rabGIT@users.noreply.github.com>
2017-06-06 11:04:58 -0700

    restaurant queries

--- restaurant_queries.js
+++ restaurant_queries.js
@@ -3,24 +3,145 @@ The MongoDB Restaurants Database
 Finding Restaurants
 
 Note that you should use projection
-for these queries to filter the returned fields to only those fields described in each problem.
+for these queries to filter the returned fields to only those fields described
+  in each problem.
 
 Note that queries from these point on may require you to use field path syntax which can be done using dot syntax like this
   "grades.grade"
 or "$grades.grade".This is done to target nested documents.
 
 Find the first 5 restaurants returning only the name
+
+db.restaurants.find({}, {
+    _id: 0,
+    name: 1
+  })
+  .limit(5)
+
 Find the name of all restaurants with at least 1 grade of A or B
+
+db.restaurants.find({
+  "grades.grade": {
+    $in: ['A', 'B']
+  }
+}, {
+  _id: 0,
+  name: 1
+})
+
 Find the name of all restaurants with at least 1 score above 20
+
+db.restaurants.find({
+  "grades.score": {
+    $gt: 20
+  }
+}, {
+  _id: 0,
+  name: 1
+})
+
 Find the unique types of cuisine in restaurants in the Bronx
+
+db.restaurants.aggregate(
+  [{
+      $match: {
+        "borough": "Bronx"
+      }
+    },
+    {
+      $group: {
+        "_id": "$cuisine"
+      }
+    }
+  ]
+);
+
 Find all the names and addresses of all the spanish restaurants in Queens
-Find all the names and addresses of all the restaurants in Manhattan that are not a Bakery, Spanish,
+
+db.restaurants.find({
+  $and: [{
+      borough: "Queens"
+    },
+    {
+      cuisine: "Spanish"
+    }
+  ]
+}, {
+  _id: 0,
+  name: 1,
+  address: 1
+})
+
+
+Find all the names and addresses of all the restaurants in Manhattan that are not a Bakery,
+  Spanish,
   Italian or Irish
-Find the name and address of the first alphabetically named Asian restaurant a grade of A
+
+db.restaurants.find({
+  $and: [{
+      borough: "Queens"
+    },
+    {
+      cuisine: {
+        $nin: ["Spanish", "Italian", "Irish"]
+      }
+    }
+  ]
+}, {
+  _id: 0,
+  name: 1,
+  address: 1
+})
+
+
+Find the name and address of the first alphabetically named Asian restaurant a grade of
+  A
+
+db.restaurants.find({
+    $where: "this.name[0] === 'A' && this.cuisine === 'Asian'"
+  }, {
+    _id: 0,
+    name: 1,
+    address: 1
+  })
+  .limit(1)
+
+
 Aggregating Restaurants
 
 List the number of restaurants under each zipcode
-List unique names of all restaurants with at least 1 grade of A or B sorted in reverse alphabetical order
+
+db.restaurants.aggregate(
+  [{
+    $group: {
+      "_id": "$address.zipcode",
+      "count": {
+        $sum: 1
+      }
+    }
+  }]
+);
+
+
+List unique names of all restaurants with at least 1 grade of A or B sorted in
+  reverse alphabetical order
+
+db.restaurants.aggregate(
+  [{
+      $match: {
+        "grades.grade": {
+          $in: ['A', 'B']
+        }
+      }
+    },
+    {
+      $group: {
+        "_id": "$name"
+      }
+    }
+  ]
+);
+
 Aggregating Restaurants with $unwind(Optional)
 
 How to Use $unwind
@@ -30,7 +151,8 @@ that they can be aggregated
   .Here is a stackoverflow post explaining $unwind with an example.
 
 Your first task is to
-try out $unwind to get used to what it does.Execute the following queries in your MongoDB shell:
+try out $unwind to get used to what it does.Execute the following queries in
+  your MongoDB shell:
 
   // ----------------------------------------
   // Remove All Containers
