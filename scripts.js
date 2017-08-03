4c4965a77ef40cc76d519a9efd2e1dba360b6ae9
Eric Glover <etgt28@mail.missouri.edu>
2017-08-03 17:58:53 -0500

    map Reduced into threads? [insert better pun here]

--- scripts.js
+++ scripts.js
@@ -187,6 +187,62 @@ db.products.aggregate([
   { $sort: { _id: 1 } }
 ]);
 
+//3. Find the number of out of stock products in each department and sort the results by the department name
+db.products.aggregate([
+  { $match: { stock: 0 } },
+  { $group: { _id: "$department", sum: { $sum: "$sales" } } },
+  { $sort: { _id: 1 } }
+]);
+
+// Map Reduce
+//Find the number of products with each color
+db.products
+  .mapReduce(
+    function() {
+      emit(this.color, 1);
+    },
+    function(keys, values) {
+      return Array.sum(values);
+    },
+    {
+      query: {},
+      out: "Number of each color"
+    }
+  )
+  .find();
+//Find the total revenue of each department (how much did each department make in sales?)
+db.products
+  .mapReduce(
+    function() {
+      emit(this.department, this.sales);
+    },
+    function(keys, values) {
+      return Array.sum(values);
+    },
+    {
+      query: {},
+      out: "Number of each color"
+    }
+  )
+  .find();
+
+//Find the potential revenue of each product (how much can each product make if the entire remaining stock is sold?)
+db.products
+  .mapReduce(
+    function() {
+      emit(this.name, this.sales * this.stock);
+    },
+    function(keys, values) {
+      return Array.sum(values);
+    },
+    {
+      query: {},
+      value: "Number of each color"
+    }
+  )
+  .find();
+
+//{ $project: { sum: 1 } }
 // db.products.find({ department: "Hardware Tools" })
 
 // db.products.update(
