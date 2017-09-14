0277f7045b62fcba07cef485e9af2b0b87f72fc7
Sean Luckett <whitecrane007@gmail.com>
2017-09-14 09:57:12 -0600

    Deletes all harware products

--- queries.js
+++ queries.js
@@ -87,3 +87,16 @@ db.products.update(
   { department: 'Hardware' },
   { $inc: { sales: 1 } }
 );
+
+// -------------------
+// Updating products
+// -------------------
+
+// 1. Remove the first product in the "Hardware" department
+db.products.remove(
+  { department: 'Hardware' },
+  { justOne: true }
+);
+
+// 2. Remove all products in the "Hardware" department
+db.products.remove({ department: 'Hardware' });
