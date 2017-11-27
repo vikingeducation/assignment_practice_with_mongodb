ef86a3ea06e99a94faa9e9c3564e5383892ab9d4
anne <richardson.ae@gmail.com>
2017-11-27 15:56:40 -0600

    Adds restaurant fields for reference

--- restaurants_queries.js
+++ restaurants_queries.js
@@ -1,4 +1,30 @@
+// ------------------------------------
+// Restaurants fields
+// ------------------------------------
+
+/* {
+  "address": {
+      "building": "1007",
+      "coord": [-73.856077, 40.848447],
+      "street": "Morris Park Ave",
+      "zipcode": "10462"
+    },
+    "borough": "Bronx",
+    "cuisine": "Bakery",
+    "grades": [
+      {"date": {"$date": 1393804800000}, "grade": "A", "score": 2},
+      {"date": {"$date": 1378857600000}, "grade": "A", "score": 6},
+      {"date": {"$date": 1358985600000}, "grade": "A", "score": 10},
+      {"date": {"$date": 1322006400000}, "grade": "A", "score": 9},
+      {"date": {"$date": 1299715200000}, "grade": "B", "score": 14}
+    ],
+    "name": "Morris Park Bake Shop",
+    "restaurant_id": "30075445"
+} */
+
+// ------------------------------------
 // Finding Restaurants
+// ------------------------------------
 // Note that queries from these point on may require you to use field path syntax which can be done using dot syntax like this "grades.grade" or "$grades.grade". This is done to target nested documents.
 
 // 1. Find the first 5 restaurants returning only the name
