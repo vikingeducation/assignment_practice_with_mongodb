59e1801d824bc44b11ccb1b01214be00d7cc9d3c
anne <richardson.ae@gmail.com>
2017-11-27 15:32:02 -0600

    Answers single purpose aggs 1-7

--- products_queries.js
+++ products_queries.js
@@ -333,15 +333,33 @@ db.products.group({
 // For each of these challenges use the Single Purpose Aggregation Operations to create a query that returns the described results.
 
 // 1. How many products are there?
+db.products.count({});
+
 
 // 2. How many products are out of stock?
+db.products.count({ stock: 0 });
+
 
 // 3. How many products are fully stocked? (100)
+db.products.count({ stock: { $gte: 100 } });
 
-// 4. How many products are almost out of stock? (>= 5)
+
+// 4. How many products are almost out of stock? (<= 5)
+db.products.count({ stock: { $lte: 5 } });
+
 
 // 5. What are all the unique names of all the departments?
+db.products.distinct('department');
+
 
 // 6. What are all the unique names of product colors?
+db.products.distinct('color');
+
 
 // 7. Find the total number of out of stock products for each department.
+db.products.group({
+  key: { department: 1 },
+  cond: { stock: 0 },
+  reduce: function(current, result) { result.count += 1; },
+  initial: { count: 0 }
+});
