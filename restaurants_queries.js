cb2469116a49646ca3ef110b74cdfa25024473a0
anne <richardson.ae@gmail.com>
2017-11-27 16:36:46 -0600

    Answers finding 1-7

--- restaurants_queries.js
+++ restaurants_queries.js
@@ -25,21 +25,68 @@
 // ------------------------------------
 // Finding Restaurants
 // ------------------------------------
-// Note that queries from these point on may require you to use field path syntax which can be done using dot syntax like this "grades.grade" or "$grades.grade". This is done to target nested documents.
+// Note that queries from this point on may require you to use field path syntax which can be done using dot syntax like this "grades.grade" or "$grades.grade". This is done to target nested documents.
 
 // 1. Find the first 5 restaurants returning only the name
+db.restaurants.find(
+  { },
+  { _id:0, name:1 }
+).limit(5);
+
 
 // 2. Find the name of all restaurants with at least 1 grade of A or B
+db.restaurants.find(
+  { $or: [
+    { 'grades.grade': 'A'},
+    { 'grades.grade': 'B'}
+  ]},
+  { _id:0, name:1 }
+);
+
 
 // 3. Find the name of all restaurants with at least 1 score above 20
+db.restaurants.find(
+  { 'grades.score': { $gt: 20} },
+  { _id:0, name:1 }
+);
+
 
 // 4. Find the unique types of cuisine in restaurants in the Bronx
+db.restaurants.distinct(
+  'cuisine',
+  { borough: "Bronx" }
+);
+
 
 // 5. Find all the names and addresses of all the Spanish restaurants in Queens
+db.restaurants.find(
+  { borough: "Queens", cuisine: 'Spanish' },
+  { _id:0, name:1, address:1 }
+).pretty();
 
-// 6. Find all the names and addresses of all the restaurants in Manhattan that are not a Bakery, Spanish, Italian or Irish
+
+// 6. Find all the names and addresses of all the restaurants in Manhattan that are not a Bakery, Spanish, Italian, or Irish
+db.restaurants.find(
+  {
+    borough: "Manhattan",
+    cuisine: { $nin: ['Bakery', 'Spanish', 'Italian', 'Irish'] }
+  },
+  { _id:0, name:1, cuisine:1, address:1 }
+).pretty();
+
 
 // 7. Find the name and address of the first alphabetically named Asian restaurant a grade of A
+db.restaurants.find(
+  { cuisine: 'Asian', 'grades.grade': 'A' },
+  { _id:0, name:1, address:1 }
+).sort({name: 1}).limit(1).pretty();
+
+// checking results
+db.restaurants.find(
+  { name: '100 Fun' },
+  { _id:0, name:1, grades:1 }
+);
+
 
 /*  --------- Aggregating Restaurants --------- */
 List the number of restaurants under each zipcode
