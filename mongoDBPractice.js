ac124c9de47f82af3b13fdb807056968bf860e88
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-12-01 14:01:59 -0500

    db practice in progress

--- mongoDBPractice.js
+++ mongoDBPractice.js
@@ -13,8 +13,7 @@ db.products.insert({
 
 //2. Insert the following products in a single query
 
-db.products.insert([
-  {
+db.products.insert([{
     name: "Screwdriver",
     price: 19.99,
     department: "Hardware",
@@ -40,56 +39,32 @@ db.products.insert([
 //1. Change the department of all products in the "Hardware" department to
 //"Hardware Tools"
 
-db.products.update(
-  { department: "Hardware" },
-  { $set: { department: "Hardware Tools" } },
-  { multi: true }
-);
+db.products.update({ department: "Hardware" }, { $set: { department: "Hardware Tools" } }, { multi: true });
 
 //2. Change the price of all products in the "Hardware Tools" department
 //to cost $10 more than their current price
 
-db.products.update(
-  { department: "Hardware Tools" },
-  { $inc: { price: 10 } },
-  { multi: true }
-);
+db.products.update({ department: "Hardware Tools" }, { $inc: { price: 10 } }, { multi: true });
 
 //3. Update the sales of all the products in the
 //"Hardware Tools" department to be at least 50
 
-db.products.update(
-  { department: "Hardware Tools", sales: { $lt: 50 } },
-  { $set: { sales: 50 } },
-  { multi: true }
-);
+db.products.update({ department: "Hardware Tools", sales: { $lt: 50 } }, { $set: { sales: 50 } }, { multi: true });
 
 //4. Change the department of all the products in
 //the "Hardware Tools" department to be "Hardware" again
 
-db.products.update(
-  { department: "Hardware Tools" },
-  { $set: { department: "Hardware" } },
-  { multi: true }
-);
+db.products.update({ department: "Hardware Tools" }, { $set: { department: "Hardware" } }, { multi: true });
 
 //5. Change the price of all the products in the
 //"Hardware" department to be $10 less than their current price
 
-db.products.update(
-  { department: "Hardware" },
-  { $inc: { price: -10 } },
-  { multi: true }
-);
+db.products.update({ department: "Hardware" }, { $inc: { price: -10 } }, { multi: true });
 
 //6. Change the sales of all the products in the
 //"Hardware" department to be at most 10
 
-db.products.update(
-  { department: "Hardware", sales: { $gt: 10 } },
-  { $set: { sales: 10 } },
-  { multi: true }
-);
+db.products.update({ department: "Hardware", sales: { $gt: 10 } }, { $set: { sales: 10 } }, { multi: true });
 
 //7. Update the first product in the "Hardware"
 //department to have one more sale
@@ -113,3 +88,49 @@ db.products.remove({ department: "Hardware" });
 //=========================
 
 //1. Find the names of all the products that are out of stock
+
+db.products.find({ stock: 0 });
+
+//2. Find the stock count of all the products with a price below $100
+
+
+db.products.find({ price: { $lt: 100 } }, { _id: 0, name: 1, stock: 1 });
+
+//Find the name, color and department of all the products with a price between $100 and $1000
+
+db.products.find({ price: { $lte: 1000, $gte: 100 } }, { _id: 0, name: 1, color: 1, department: 1 });
+
+//Find the names of all the red products
+
+db.products.find({ color: "red" }, { _id: 0, name: 1 })
+
+//Find only the IDs of all the red and blue products
+
+db.products.find({
+  $or: [
+    { color: "red" },
+    { color: "blue" }
+  ]
+}, { _id: 1 })
+
+//Find the names of all the products that are not red or blue
+
+db.products.find({
+  $and: [
+    { color: { $ne: "red" } },
+    { color: { $ne: "blue" } }
+  ]
+}, { _id: 1, color: 1 })
+
+//Find the names of all the products that 
+//are not in the Sports or Games departments
+
+db.products.find({ $where: "this.department!= 'Sports' && this.department!= 'Games'" }, { _id: 0, name: 1, department: 1 })
+
+//Find the name and price of all the products with names that
+// begin with the letter F and end with the letter S and ignore case
+
+db.products.find({ name: { $regex: /^F*+S$/i } },
+
+
+)
