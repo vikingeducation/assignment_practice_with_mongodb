a289bff182e85f213635cb080cebe05a22944bb2
EdwinYung <eyung7@gmail.com>
2017-12-01 19:01:12 -0500

    working on products

--- products.js
+++ products.js
@@ -52,6 +52,13 @@ db.products.insert(multiple);
 // Note for some of these you may have to refer to update operators like $min and $max.
 //
 // Change the department of all products in the "Hardware" department to "Hardware Tools"
+db.products.update(
+  {},
+  { $rename: { "Hardware Tools": "department" } },
+  { multi: true }
+);
+
+
 
 db.products.update(
   { department: "Hardware" },
@@ -139,37 +146,230 @@ db.products.find(
 db.products.find({ color: "red" }, { _id: 0, name: 1 });
 
 // Find only the IDs of all the red and blue products
-db.products.find({{ color: "red" }, {color:'blue'} }, { _id: 0, name: 1 });
+db.products.find(
+  {
+    $or: [{ color: "red" }, { color: "blue" }]
+  },
+  {
+    _id: 0,
+    name: 1,
+    color: 1
+  }
+);
 
 // Find the names of all the products that are not red or blue
+db.products.find(
+  {
+    $and: [{ color: { $ne: "red" } }, { color: { $ne: "blue" } }]
+  },
+  {
+    _id: 0,
+    name: 1,
+    color: 1
+  }
+);
 
 // Find the names of all the products that are not in the Sports or Games departments
+db.products.find(
+  {
+    $and: [{ department: { $ne: "Sports" } }, { department: { $ne: "Games" } }]
+  },
+  {
+    _id: 0,
+    name: 1,
+    department: 1
+  }
+);
 
 // Find the name and price of all the products with names that begin with the letter F and end with the letter S and ignore case
 
+db.products.find(
+  {
+    name: { $regex: /^[F].*[S]$/, $options: "i" }
+  },
+  {
+    _id: 0,
+    name: 1,
+    price: 1
+  }
+);
+
 // Using $where, find all the product names that begin with T
 
+db.products.find(
+  {
+    $where: function() {
+      return this.name[0] === "T";
+    }
+  },
+  {
+    _id: 0,
+    name: 1
+  }
+);
+
 // Using $where, find all the product names that begin with capital F or end with lowercase S
+
+db.products.find(
+  {
+    $where: function() {
+      return this.name[0] === "F" || this.name[-1] === "s";
+    }
+  },
+  {
+    _id: 0,
+    name: 1
+  }
+);
+
 // Using $where, find all the product names that begin with capital T and have a price less than $100
+
+db.products.find(
+  {
+    $where: function() {
+      return this.name[0] === "T" && this.price < 100;
+    }
+  },
+  {
+    _id: 0,
+    name: 1,
+    price: 1
+  }
+);
 
 // Using $where, find all the product names and prices of products that either start with A and have a price of at least $100 or start with B and have a price of at most $100
 
-//
+db.products.find(
+  {
+    $where: function() {
+      return (
+        (this.name[0] === "A" && this.price >= 100) ||
+        (this.name[0] === "B" && this.price <= 100)
+      );
+    }
+  },
+  {
+    _id: 0,
+    name: 1,
+    price: 1
+  }
+);
 // With the Aggregation Pipeline
 //
 // For each of these challenges use the Aggregation Pipeline to create a query that returns the described results.
 //
 // Find the total number of sales each department made and sort the results by the department name
+db.products.aggregate([
+  {
+    $group: {
+      _id: "$department",
+      totalSales: { $sum: "$sales" }
+    }
+  },
+  {
+    $sort: {
+      _id: 1
+    }
+  },
+  {
+    $project: {
+      _id: 1,
+      totalSales: 1
+    }
+  }
+]);
+
 // Find the total number of sales each department made of a product with a price of at least $100 and sort the results by the department name
+db.products.aggregate([
+  {
+    $group: {
+      _id: "$department",
+      totalSales: { $sum: "$sales" }
+    }
+  },
+  {
+    $sort: {
+      _id: 1
+    }
+  },
+  {
+    $project: {
+      _id: 1,
+      totalSales: 1
+    }
+  }
+]);
+
 // Find the number of out of stock products in each department and sort the results by the department name
+
+db.products.aggregate([
+  {
+    $group: {
+      _id: "$department",
+      outofstock: { stock: { $lt: 1 } }
+    }
+  },
+  {
+    $sort: {
+      _id: 1
+    }
+  },
+  {
+    $project: {
+      _id: 1,
+      outofstock: 1
+    }
+  }
+]);
+
 // With Map-Reduce
 //
 // For each of these challenges use the Map-Reduce to create a query that returns the described results.
 //
 // Find the number of products with each color
+db.products.mapReduce(
+    function() {
+      emit(this.color, 1);
+    },
+    function(keys, values) {
+      return Array.sum(values);
+    },
+    {
+      query: {},
+      out: {inline:1}
+    }
+  )
+
 // Find the total revenue of each department (how much did each department make in sales?)
+db.products.mapReduce(
+    function() {
+      emit(this.department, this.sales);
+    },
+    function(keys, values) {
+      return Array.sum(values);
+    },
+    {
+      query: {},
+      out: {inline:1}
+    }
+  )
+
 // Find the potential revenue of each product (how much can each product make if the entire remaining stock is sold?)
+db.products.mapReduce(
+    function() {
+      emit(this.stock, this.price);
+    },
+    function(keys, values) {
+      return Array.sum(values * keys);
+    },
+    {
+      query: {},
+      out: {inline:1}
+    }
+  )
+
 // Find the sum of the total and potential revenue for each product
+
 // With Single Purpose Aggregation Operations
 //
 // For each of these challenges use the Single Purpose Aggregation Operations to create a query that returns the described results.
